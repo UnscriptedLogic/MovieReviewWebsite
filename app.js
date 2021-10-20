34241c2dd98d0efe9d08df2ce4517e53c0c7fc38
Nathaniel Alcala <semblancewarden@Gmail.com>
2021-10-21 02:57:20 +0800

    Branching Away from YouTube Tutorial. Custom Animations, GUI Layouts and Colours

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
+//element.addEventListener('click', function())
+
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
