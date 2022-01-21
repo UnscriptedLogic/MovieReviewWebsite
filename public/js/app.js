950133deaa216c6f027c58f74477aa807228d87f
LaptopNat <semblancewarden@Gmail.com>
2022-01-21 13:23:47 +0800

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
+var restaurant_url = "/movies";
+var restaurant_array = []; // This creates an empty movie array
+var restaurantCount = 0;
+
+var currentIndex = 0;
+var comment_url = "/comments";
+var comment_array = [] //creates an empty array 
+
+
 document.addEventListener("click", mouseclick => {
     const isDropDownButton = mouseclick.target.matches("[data-dropdown-button]");
 
